2d89350d58a20580203c8e0c601e17e5cf1978a4
Michael Meding <mmeding@Michaels-Mac-Studio.local>
2026-05-08 18:44:48 -0300

    fix: adapt sandbox exec kills to Signal API

--- Packages/OsaurusCore/Services/Sandbox/SandboxManager.swift
+++ Packages/OsaurusCore/Services/Sandbox/SandboxManager.swift
@@ -1006,7 +1006,7 @@
             // separate process registry.
             if let onProcessStarted {
                 let handle = ProcessHandle(pid: process.pid) { signal in
-                    try await process.kill(signal)
+                    try await process.kill(Signal(rawValue: signal))
                 }
                 onProcessStarted(handle)
             }
@@ -1076,7 +1076,7 @@
                     return status
                 }
 
-                try? await process.kill(15)
+                try? await process.kill(.term)
                 throw SandboxError.timeout
             }
         }
