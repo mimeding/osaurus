beaef939fa0be5e23e9925ef3d774d60796a6e80
Michael Meding <264272563+mimeding@users.noreply.github.com>
2026-05-17 10:09:43 -0300

    Add XLSX read adapter groundwork
    (cherry picked from commit 2846b18e6da0b9e6f9449da8febff35a8cd2320b)

--- Packages/OsaurusCore/Managers/Documents/DocumentAdaptersBootstrap.swift
+++ Packages/OsaurusCore/Managers/Documents/DocumentAdaptersBootstrap.swift
@@ -29,6 +29,7 @@ public enum DocumentAdaptersBootstrap {
         registry.register(adapter: CSVAdapter(delimiter: .tab))
         registry.register(adapter: PDFAdapter())
         registry.register(adapter: RichDocumentAdapter())
+        registry.register(adapter: XLSXAdapter())
         if registry === DocumentFormatRegistry.shared {
             didRegisterShared = true
         }
